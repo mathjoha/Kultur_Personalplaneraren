a0e9ad3df5db5e74b8ed83fbe0f643c7c9b500de
mathjoha <MathiasJoha@gmail.com>
2024-09-27 17:52:22 +0200

    Add observable code

---
+++ observablehq.config.js
@@ -0,0 +1,29 @@
+// See https://observablehq.com/framework/config for documentation.
+export default {
+  // The project’s title; used in the sidebar and webpage titles.
+  title: "Personal Counter",
+
+
+
+  // Content to add to the head of the page, e.g. for a favicon:
+  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+
+  // The path to the source root.
+  root: "src",
+
+  // Some additional configuration options and their defaults:
+  // theme: "default", // try "light", "dark", "slate", etc.
+  // header: "", // what to show in the header (HTML)
+  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  // sidebar: true, // whether to show the sidebar
+  // toc: true, // whether to show the table of contents
+  // pager: true, // whether to show previous & next links in the footer
+  // output: "dist", // path to the output root for build
+  // search: true, // activate search
+  // linkify: true, // convert URLs in Markdown to links
+  // typographer: false, // smart quotes and other typographic improvements
+  // cleanUrls: true, // drop .html from URLs
+
+  footer: '<div style="float: left">© 2024 Mathias Johansson</div><div style="float: right" > Built with observable</div><div style="margin: 0 auto; width: 100px;"><a href="https://github.com/mathjoha/Kultur_Personalplaneraren/issues/new">Report Issue<a></div>'
+
+};
