ed10d378ca2c84cb76b82f3e2248b9a15f1115ce
M~Johansson <MathiasJoha@gmail.com>
2024-10-22 15:12:48 +0200

    --amend

---
+++ tests/divide_percentages.test.js
@@ -0,0 +1,30 @@
+import divideHours from "../src/scripts/main.js";
+import assert from "assert";
+
+it('Sum to 100 w/o external funds', () => {
+    const title = { 'base': 0.1, "research": 0.3, "teaching": 0.7, "dev": 0 };
+    const sum = divideHours(title, 0).reduce((a, b) => (a + b), 0);
+    assert.equal(sum, 100);
+
+});
+
+
+it('Sum to 100', () => {
+    const title = { 'base': 0.1, "research": 0.3, "teaching": 0.7, "dev": 10 };
+    const sum = divideHours(title, 0).reduce((a, b) => (a + b), 0);
+    assert.equal(sum, 100);
+
+});
+
+it('Lower teaching threshold', () => {
+    const title = { 'base': 0.1, "research": 0.3, "teaching": 0.7, "dev": 0 };
+    const teaching = divideHours(title, 0)[1]
+    assert.ok(teaching >= 20);
+});
+
+
+it('Lower teaching threshold with tons of dev', () => {
+    const title = { 'base': 0.1, "research": 0.3, "teaching": 0.7, "dev": 10 };
+    const teaching = divideHours(title, 0)[1]
+    assert.equal(teaching, 20);
+});
