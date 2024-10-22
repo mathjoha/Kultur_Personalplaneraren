fbc1790fb62208cbc2f8f356272c4dc27189014e
M~Johansson <MathiasJoha@gmail.com>
2024-10-22 17:23:42 +0200

    Test for titles to add up to 100%

--- src/scripts/titles.js
+++ src/scripts/titles.js
@@ -2,6 +2,6 @@ export default [
     ['Lektor', { 'base': 0.1, "research": 0.2, "teaching": 0.7 }],
     ['Lektor med docentkompetens', { 'base': 0.1, "research": 0.25, "teaching": 0.65 }],
     ['Professor', { 'base': 0.05, "research": 0.51, "teaching": 0.44 }],
-    ['Biträdande Lektor', { 'base': 0.05, "research": 0.51, "teaching": 0.49 }],
+    ['Biträdande Lektor', { 'base': 0.05, "research": 0.51, "teaching": 0.44 }],
     ['Adjunkt', { 'base': 0.1, "research": 0.1, "teaching": 0.80 }],
 ]
