7e22e574df144dc0947c70ecd6cac64c1b7ae053
M~Johansson <MathiasJoha@gmail.com>
2024-10-22 17:18:44 +0200

    Remove dev from data

--- src/scripts/titles.js
+++ src/scripts/titles.js
@@ -1,7 +1,7 @@
 export default [
-    ['Lektor', { 'base': 0.1, "research": 0.2, "teaching": 0.7, "dev": 0 }],
-    ['Lektor med docentkompetens', { 'base': 0.1, "research": 0.25, "teaching": 0.65, "dev": 0 }],
-    ['Professor', { 'base': 0.05, "research": 0.51, "teaching": 0.44, "dev": 0 }],
-    ['Biträdande Lektor', { 'base': 0.05, "research": 0.51, "teaching": 0.49, "dev": 0 }],
-    ['Adjunkt', { 'base': 0.1, "research": 0.1, "teaching": 0.80, "dev": 0.1 }],
+    ['Lektor', { 'base': 0.1, "research": 0.2, "teaching": 0.7 }],
+    ['Lektor med docentkompetens', { 'base': 0.1, "research": 0.25, "teaching": 0.65 }],
+    ['Professor', { 'base': 0.05, "research": 0.51, "teaching": 0.44 }],
+    ['Biträdande Lektor', { 'base': 0.05, "research": 0.51, "teaching": 0.49 }],
+    ['Adjunkt', { 'base': 0.1, "research": 0.1, "teaching": 0.80 }],
 ]
