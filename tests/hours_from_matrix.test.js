5ecf5712f48bfc765235ccf475390a30968b6ec5
M~Johansson <MathiasJoha@gmail.com>
2024-10-22 09:00:00 +0200

    Test basic hours calculation

---
+++ tests/hours_from_matrix.test.js
@@ -0,0 +1,38 @@
+const addHours = require('../src/scripts/addHours');
+
+test('Calculate 1 row', () => {
+    const data_in = [{ 'percent': 10 }]
+    const hrs = 1000
+
+    const data_target = [{ 'percent': 10, 'hours': 100 }];
+
+    const data_out = addHours(data_in, hrs);
+
+    console.log(data_out)
+
+    expect(data_out).toEqual(data_target)
+
+});
+
+
+test('Calculate 3 rows', () => {
+    const data_in = [
+        { 'percent': 10 },
+        { 'percent': 100 },
+        { 'percent': 50 },
+    ]
+    const hrs = 1700
+
+    const data_target = [
+        { 'percent': 10, 'hours': 170 },
+        { 'percent': 100, 'hours': 1700 },
+        { 'percent': 50, 'hours': 850 },
+    ];
+
+    const data_out = addHours(data_in, hrs);
+
+    console.log(data_out)
+
+    expect(data_out).toEqual(data_target)
+
+});
