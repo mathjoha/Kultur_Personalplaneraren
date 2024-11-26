cee9d91e61521092bd8838a41fdc6efe24e12e20
M~Johansson <MathiasJoha@gmail.com>
2024-11-27 00:32:35 +0100

    Lock other %

---
+++ src/scripts/part.time.js
@@ -0,0 +1,13 @@
+export default function minPartTime(title, hours) {
+    // Calculates the minimum allowed part time based
+    // on the number of hours, fixed-portion of other-time
+    // relative portion of teaching and research
+
+    const fixed_other = Math.round(title.base * hours)
+
+    const min_hrs = fixed_other / (1 - 0.2 - title.research)
+
+    const min_percent = min_hrs * 100 / hours
+    return Math.ceil(min_percent)
+
+}
