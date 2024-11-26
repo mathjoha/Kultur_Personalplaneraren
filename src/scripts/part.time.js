8e5626ee7d185440a0e0c2eb6fa1a9494af8d057
M~Johansson <MathiasJoha@gmail.com>
2024-11-26 21:30:56 +0100

    fix typo in docstring

--- src/scripts/part.time.js
+++ src/scripts/part.time.js
@@ -1,6 +1,6 @@
 export default function minPartTime(title, hours) {
     // Calculates the minimum allowed part time based
-    // on the number of hours, fixed-portion oof other-time
+    // on the number of hours, fixed-portion of other-time
     // relative portion of teaching and research
 
     const fixed_other = Math.round(title.base * hours)
