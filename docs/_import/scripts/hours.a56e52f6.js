ed176b5eabcb2aff7353d39484e268ca7189bfc4
M~Johansson <MathiasJoha@gmail.com>
2024-11-26 23:12:27 +0100

    Update version number

---
+++ docs/_import/scripts/hours.a56e52f6.js
@@ -0,0 +1,7 @@
+export default function addHours(data, hrs) {
+    function calculateHours(item, index, arr) {
+        arr[index]['hours'] = Math.round(arr[index]['percent'] * hrs / 100);
+    }
+    data.forEach(calculateHours)
+    return data
+}
--- src/index.md
+++ src/index.md
@@ -3,7 +3,7 @@ toc: false
 ---
 
 ```js
-const version =  'v1.0.3'
+const version =  'v1.1.0'
 ```
 
 <div style="float: right" >${version}</div>
