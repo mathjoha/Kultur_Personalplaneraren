3b333fe28ab641a54dd3c2f8adeb44bb3342d418
M~Johansson <MathiasJoha@gmail.com>
2024-11-26 19:48:48 +0100

    WIP add natural_base to parametrized test

--- tests/parametrize_division.test.js
+++ tests/parametrize_division.test.js
@@ -3,13 +3,20 @@ import assert from "assert";
 
 describe('Parametrize hours', () => {
     [0.05, 0.1, 0.15].forEach((base) => {
-        const target_base = Math.round(base * 100);
+        const target_base = Math.round(base * 10000) / 100;
         for (let teach = 0.2; teach <= 0.7; teach += 0.05) {
-            let research = 1 - base - teach;
+            let research = Math.round(1000 * (1 - base - teach)) / 10000;
             const target_research = Math.round(research * 100);
             for (let bidrag = 0; bidrag <= 100 - base * 100 - 20; bidrag += 5) {
-                const title = { 'base': base, "research": research, "teaching": teach, "dev": 0 };
-                const result = divideHours(title, bidrag);
+
+                const title = {
+                    'base': base,
+                    "research": research,
+                    "teaching": teach,
+                    "natural_base": base,
+                };
+
+                const result = divideHours(title, Math.round(10000 * bidrag) / 100);
                 const total_research = 100 - result[0] - result[1]
                 describe(`Inputs: base: ${target_base}, teaching: ${Math.round(teach * 100)}. research: ${target_research}, bidrag: ${bidrag}`, () => {
                     it(`Sum to 100`, () => {
